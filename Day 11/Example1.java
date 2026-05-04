dbac3c3ed97fee9f55689a19d11a5db334928d1b
Sabbir Mahatab <sabbir107031@gmail.com>
2026-05-04 09:52:35 +0600

    Update Example1.java

--- Day 11/Example1.java
+++ Day 11/Example1.java
@@ -1,7 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
-public class Example2 {
+public class Example1 {
     public static void main(String[] args){
         LocalDateTime now = LocalDateTime.now();
         System.out.println("Current date and time: " + now);
